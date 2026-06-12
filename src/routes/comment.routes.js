64f032bcda51ffbbd75dc397ef9ba857c732d106
elianmsalas <emunozsalas@icloud.com>
2026-06-11 23:41:22 -0400

    Se agrega validación en rutas

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/comment.controller");
 const { protect } = require("../middlewares/auth.middleware");
+const { validate } = require("../middlewares/error.middleware");
+const { createCommentSchema, updateCommentSchema } = require("../validations/comment.validation");
 
 // Obtener comentarios de un post (público)
 router.get("/posts/:postId/comments", commentController.getCommentsByPost);
 
 // Crear comentario en un post (protegido)
-router.post("/posts/:postId/comments", protect, commentController.createComment);
+router.post("/posts/:postId/comments", protect, validate(createCommentSchema), commentController.createComment);
 
 // Editar y eliminar comentario (protegido)
-router.patch("/comments/:commentId", protect, commentController.updateComment);
+router.patch("/comments/:commentId", protect, validate(updateCommentSchema), commentController.updateComment);
 router.delete("/comments/:commentId", protect, commentController.deleteComment);
 
 module.exports = router;
