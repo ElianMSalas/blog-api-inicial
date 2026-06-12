acd73993b0312ab1a7ad2cd1845b8ca1112ee781
elianmsalas <emunozsalas@icloud.com>
2026-06-11 23:32:09 -0400

    Se agrega línea que termina conexión si no se conecta a DB

--- src/config/db.js
+++ src/config/db.js
@@ -5,9 +5,10 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB conectado: ${conn.connection.host}`);
     } catch (error) {
-    console.error("Error completo:");
-    console.error(error);
-}
+        console.error("Error al conectar a MongoDB");
+        console.error(error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
